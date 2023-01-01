8c384ebbc11d1c44ce1a6c0113eb57762f206dd4
Alejandro Cerezo <alce65@hotmail.es>
2023-01-01 17:40:21 +0100

    Add test for aLength function

---
+++ src/03.Array.Functions/array.functions.test.js
@@ -0,0 +1,19 @@
+describe('Given aLength function', () => {
+  describe('When we invoque it with an empty array', () => {
+    // beforeEach(() => {})
+    test('Then it return should be 0', () => {
+      // arrange
+      const aData = [];
+      // act
+      const r = aLength(aData);
+      // assert
+      expect(r).toBe(0);
+    });
+  });
+
+  describe('When we invoque it with a n items array', () => {
+    test('Then it return should be n', () => {
+      //
+    });
+  });
+});
