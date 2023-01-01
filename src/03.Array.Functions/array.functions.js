fb578a179209277007f4bb87d62756a26482dc95
Alejandro Cerezo <alce65@hotmail.es>
2023-01-01 18:34:29 +0100

    Fix eslint rules exclusions

--- src/03.Array.Functions/array.functions.js
+++ src/03.Array.Functions/array.functions.js
@@ -2,6 +2,7 @@ export const aLength = (array = []) => {
   const keys = Object.keys(array);
   let i = 0;
   while (keys[i]) {
+    // eslint-disable-next-line no-plusplus
     i++;
   }
   return i;
