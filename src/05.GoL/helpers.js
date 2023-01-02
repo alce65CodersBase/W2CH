0461f4ce751bd8c7110c6ecef8f3d4227eab11b9
Alejandro Cerezo <alce65@hotmail.es>
2023-01-02 11:54:49 +0100

    Refactor for reduce algorithm complexity

--- src/05.GoL/helpers.js
+++ src/05.GoL/helpers.js
@@ -1,4 +1,30 @@
-/* eslint-disable arrow-body-style */
+// Previous version: complexity 8
+// export const findNeighborPositions = (i = 0, j = 0, array = []) => {
+//   // posiciones al rededor de (i,j)
+//   // array[i-1][j-1],array[i-1][j],array[i-1][j+1],
+//   // array[i][j-1],array[i][j],array[i][j+1],
+//   // array[i+1][j-1],array[i+1][j],array[i+1][j+1]
+//   if (!array.length) return [];
+//   const max = array.length - 1;
+//   const possiblePositions = [
+//     [i - 1, j - 1],
+//     [i - 1, j],
+//     [i - 1, j + 1],
+//     [i, j - 1],
+//     [i, j + 1],
+//     [i + 1, j - 1],
+//     [i + 1, j],
+//     [i + 1, j + 1],
+//   ];
+
+//   return possiblePositions.filter(
+//     (item) => item[0] >= 0 && item[1] >= 0 && item[0] <= max && item[1] <= max
+//   );
+// };
+
+const filterValidPosition = (item, max) =>
+  item[0] >= 0 && item[1] >= 0 && item[0] <= max && item[1] <= max;
+
 export const findNeighborPositions = (i = 0, j = 0, array = []) => {
   // posiciones al rededor de (i,j)
   // array[i-1][j-1],array[i-1][j],array[i-1][j+1],
@@ -17,28 +43,52 @@ export const findNeighborPositions = (i = 0, j = 0, array = []) => {
     [i + 1, j + 1],
   ];
 
-  return possiblePositions.filter(
-    (item) => item[0] >= 0 && item[1] >= 0 && item[0] <= max && item[1] <= max
-  );
+  return possiblePositions.filter((item) => filterValidPosition(item, max));
 };
+
+// Previous version: complexity 6
+// export function countAliveNeighbors(i = 0, j = 0, array = []) {
+//   if (!array.length) return 0;
+//   const validPositions = findNeighborPositions(i, j, array);
+//   const values = validPositions.map((item) => array[item[0]][item[1]]);
+//   return values.reduce((a, b) => a + b);
+// }
+
+const positionsToValues = (positions, array) =>
+  positions.map((item) => array[item[0]][item[1]]);
 
 export function countAliveNeighbors(i = 0, j = 0, array = []) {
   if (!array.length) return 0;
   const validPositions = findNeighborPositions(i, j, array);
-  return validPositions
-    .map((item) => array[item[0]][item[1]])
-    .reduce((a, b) => a + b);
+  const values = positionsToValues(validPositions, array);
+  return values.reduce((a, b) => a + b);
 }
+
+// Previos version: complexity 9
+// export function willBeAlive(i = 0, j = 0, array = []) {
+//   if (!array.length) return 0;
+//   const countValidCells = countAliveNeighbors(i, j, array);
+//   if (array[i][j] === 1) {
+//     // starting alive
+//     return countValidCells > 2 && countValidCells <= 3 ? 1 : 0;
+//   }
+//   // starting dead
+//   return countValidCells >= 3 ? 1 : 0;
+// }
+
+const forLiveCells = (countValidCells) =>
+  countValidCells > 2 && countValidCells <= 3 ? 1 : 0;
+
+const forDeadCells = (countValidCells) => (countValidCells >= 3 ? 1 : 0);
 
 export function willBeAlive(i = 0, j = 0, array = []) {
   if (!array.length) return 0;
   const countValidCells = countAliveNeighbors(i, j, array);
-  if (array[i][j] === 1) {
-    // starting alive
-    return countValidCells > 2 && countValidCells <= 3 ? 1 : 0;
-  }
-  // starting dead
-  return countValidCells >= 3 ? 1 : 0;
+  return array[i][j] === 1
+    ? // starting alive
+      forLiveCells(countValidCells)
+    : // starting dead
+      forDeadCells(countValidCells);
 }
 
 export function liveCycle(array) {
