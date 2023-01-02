e1a4cf28e3d17e88459cbc0f36cf0315b93f8bbb
Alejandro Cerezo <alce65@hotmail.es>
2023-01-02 10:59:14 +0100

    Refactor findNeighborPositions for fulfill the test cases

--- src/05.GoL/helpers.js
+++ src/05.GoL/helpers.js
@@ -5,7 +5,8 @@ export const findNeighborPositions = (i = 0, j = 0, array = []) => {
   // array[i][j-1],array[i][j],array[i][j+1],
   // array[i+1][j-1],array[i+1][j],array[i+1][j+1]
   if (!array.length) return [];
-  return [
+  const max = array.length - 1;
+  const possiblePositions = [
     [i - 1, j - 1],
     [i - 1, j],
     [i - 1, j + 1],
@@ -15,4 +16,8 @@ export const findNeighborPositions = (i = 0, j = 0, array = []) => {
     [i + 1, j],
     [i + 1, j + 1],
   ];
+
+  return possiblePositions.filter(
+    (item) => item[0] >= 0 && item[1] >= 0 && item[0] <= max && item[1] <= max
+  );
 };
